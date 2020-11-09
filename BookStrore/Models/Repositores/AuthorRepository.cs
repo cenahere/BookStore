da028312e3c628be71e848889ae60413a1b68e64
محمد حميدة <hero691986@gmail.com>
2020-11-09 19:58:15 +0200

    Add project files.

---
+++ BookStrore/Models/Repositores/AuthorRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStrore.Models.Repositores
+{
+    public class AuthorRepository : IBookStroreRepository<Author>
+    {
+        IList<Author> authors;
+        public AuthorRepository()
+        {
+            authors = new List<Author>()
+           {
+               new Author
+               {Id = 1 , FullName = "Ali Sayed" },
+               new Author   {Id=2,FullName="Ahmed Mohamed"},
+               new Author {Id=3,FullName="Sayed Galal"}
+           };
+        }
+        public void Add(Author entity)
+        {
+            authors.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var author = Find(id);
+            authors.Remove(author);
+        }
+
+        public Author Find(int id)
+        {
+            var author = authors.SingleOrDefault(a => a.Id == id);
+            return author;
+        }
+
+        public IList<Author> List()
+        {
+            return authors;
+        }
+
+        public void Update(Author newAuthor, int id)
+        {
+            var author = Find(id);
+            author.FullName = newAuthor.FullName;
+        }
+    }
+}
---
+++ BookStrore/Models/Repositores/BookRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStrore.Models.Repositores
+{
+    public class BookRepository : IBookStroreRepository<Book>
+    {
+        List<Book> books;
+        public BookRepository()
+        {
+            books = new List<Book>()
+            {
+                new Book
+                {
+                    Id=1, Title = "C# Programming" , Description="no Description"
+                },
+                new Book
+                {
+                   Id=2, Title = "Php Programming" , Description="no Description",
+
+                },
+                new Book
+                {
+                   Id=3, Title = "Node Programming" , Description="no Description",
+                 }
+            };
+        }
+        public void Add(Book entity)
+        {
+            books.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var book = Find(id);
+            books.Remove(book);
+        }
+
+        public Book Find(int id)
+        {
+            var book = books.SingleOrDefault(x=>x.Id==id);
+            return book;
+        }
+
+        public IList<Book> List()
+        {
+            return books;
+        }
+
+        public void Update(Book newBook , int id)
+        {
+            var book = Find(id);
+            book.Title = newBook.Title;
+            book.Author = newBook.Author;
+            book.Description = newBook.Description;
+
+        }
+    }
+}
